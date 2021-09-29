181b089c861c596dba12ee515a60ee13368b74bd
Guruswami <guru.h121@gmail.com>
2021-09-29 21:22:04 +0530

    Added location

--- src/pages/RouterPage.js
+++ src/pages/RouterPage.js
@@ -1,13 +1,13 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Home from './Home';
+// import Home from './Home';
 import Location from './Location'
 
 const RouterPage = () => {
     return (
         <BrowserRouter>
             <Switch>
-                <Route exact path='/location' component={Location} />
-                <Route exact path='*' component={Home} />
+                {/* <Route exact path='/location' component={Location} /> */}
+                <Route exact path='*' component={Location} />
             </Switch>
         </BrowserRouter>
     )
